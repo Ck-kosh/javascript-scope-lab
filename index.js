04970409b33a21c66e7e57c02bdc69b4458aec68
Ck-kosh <elias.cheruiyot@student.moringaschool.com>
2026-04-15 17:18:20 +0300

    second commit

--- index.js
+++ index.js
@@ -31,3 +31,21 @@ if (true) {
 function changeFeaturedDrink() {
   featuredDrink = "The JavaShake";
 }
+// GLOBAL SCOPE VARIABLES
+const burgers = ["Hamburger", "Cheeseburger"];
+
+let featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+  const newBurger = "Flatburger";
+  burgers.push(newBurger);
+}
+
+if(true){
+const anotherNewBurger = "Maple Bacon Burger";
+burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+}
